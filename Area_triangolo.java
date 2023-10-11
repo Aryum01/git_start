b95d3ec0d6980836474543eeceaecc77447a1974
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-11 17:54:20 +0200

    Add files via upload

---
+++ Area_triangolo.java
@@ -0,0 +1,9 @@
+public class Area_triangolo{
+    public static void main(String arg[]){
+       double base = 5;
+       double altezza = 6;
+       double   area = (base * altezza)/2;
+
+       System.out.println("ecco l'area del triangolo :" +  area);
+    }
+}
