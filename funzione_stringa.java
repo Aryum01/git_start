aedae9272de225b5785d1f1a3cac9789e3d03ef9
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-11 18:45:11 +0200

    Add files via upload

---
+++ funzione_stringa.java
@@ -0,0 +1,11 @@
+public class funzione_stringa {
+    public static void main(String arg[]){
+        String b = "ciao";
+        lunghezza_stringa(b);
+       }
+   
+   
+       public static void lunghezza_stringa(String b){ 
+           System.out.println("la lunghezza della stringa è "+ b.length());
+   }
+}
