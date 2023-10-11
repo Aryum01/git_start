9de366ec6e3267df68bb7f1c749966f3d4bd6173
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-11 18:22:10 +0200

    Add files via upload

---
+++ Casting_double.java
@@ -0,0 +1,8 @@
+public class Casting_double {
+    public static void main(String arg[]){
+        double a = 2.0;
+        double b = 3.0;
+        int somma = (int)a + (int)b;
+        System.out.println("la somma tra a e b è :" + somma );
+    }
+}
