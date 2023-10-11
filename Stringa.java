7a9dd31963c3d45ae9de4a33d0f60629eaa4735d
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-11 18:11:10 +0200

    Add files via upload

---
+++ Stringa.java
@@ -0,0 +1,11 @@
+public class Stringa {
+    public static void main(String arg[]){
+        char a ='c';
+        char b ='i';
+        char c ='a';
+        char d ='o';
+        System.out.println( "" + a + b + c + d);
+        
+
+    }
+}
