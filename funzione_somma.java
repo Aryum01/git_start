93618d7d63eef577f968247d3a4ba510f7d38b75
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-11 18:35:26 +0200

    Add files via upload

---
+++ funzione_somma.java
@@ -0,0 +1,12 @@
+public class funzione_somma {
+    public static void main(String arg[]){
+     System.out.println("somma di due valori " + somma(1,2));
+    }
+
+
+    public static int  somma (int a,int b){ 
+        return a + b; 
+}
+}
+
+
