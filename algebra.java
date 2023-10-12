059eb32ddfaf37cf5a1db3cfa37fe55870c0e2e9
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-12 16:05:03 +0200

    Add files via upload

---
+++ algebra.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class algebra {
+     public static void main(String arg[]){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("inserisci un operatore algebrico");
+        String number =scan.nextLine();
+        scan.close();
+        operator(number);
+    }
+
+    public static void operator(String a){
+        switch(a){
+           case "+" : System.out.println("addizione");
+           break;
+           case "-" : System.out.println("sottrazione");
+           break;
+           case "*": System.out.println("moltiplicazione");
+           break;
+           case "/" : System.out.println("divisione");
+           break;
+           default: System.out.println("input non valido");
+        }
+        
+    }
+}
