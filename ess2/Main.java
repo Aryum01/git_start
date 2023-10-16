e86b4cc890f9cbe396123841112bde41ccb3249c
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-16 21:54:15 +0200

    Add files via upload

---
+++ ess2/Main.java
@@ -0,0 +1,8 @@
+package modulo2.ess2;
+
+public class Main{
+    public static void main(String[] args){
+     Auto auto1 = new Auto (1500, "ff254ff","mazda", "2");
+     System.out.println(auto1);
+ }
+    }
