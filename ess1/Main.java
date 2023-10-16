72cf0e49a11098095264ea6ff256367ae1ca22a4
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-16 20:20:04 +0200

    Add files via upload

---
+++ ess1/Main.java
@@ -0,0 +1,12 @@
+package modulo2.ess1;
+public class Main{
+    public static void main(String[] args){
+     Studente studente1 = new Studente("Mario","Rossi", 423412);
+     Studente studente2 = new Studente("Gino","Bianchi", 325167);
+     Studente studente3 = new Studente("Ugo","Verdi", 891675);
+
+     String tuttiGliStudenti = studente1 + "\n" + studente2 + "\n" + studente3;
+     System.out.println(tuttiGliStudenti);
+ }
+    }
+
---
+++ ess1/Studente.java
@@ -0,0 +1,52 @@
+package modulo2.ess1;
+
+public class Studente {
+    private String nome;
+    private String cognome;
+    private int id;
+
+
+    public Studente(String nome , String cognome , int id){
+      this.nome = nome;
+      this.cognome = cognome;
+      this.id = id;
+}
+
+    public String getnome(){
+        return nome;
+    }
+
+    public void setnome( String newName){
+       nome = newName;
+    }
+
+
+     public String getcognome(){
+        return nome;
+    }
+
+    public void setcognome( String newSurname){
+       cognome = newSurname;
+    }    
+
+
+     public int getid(){
+        return id;
+    }
+
+    public void setid( int newId){
+       id = newId;
+    }  
+    
+    @Override
+    public String toString(){
+        return "Studente{" +
+        "nome = " + nome + ", " + 
+        "cognome = " + cognome + 
+        ", id = "+ id + '}';
+            
+        
+    }
+
+}
+
