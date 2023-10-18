2d156e8100f7a17aaf132fbe094186f6d2969e67
Aryum01 <147306723+Aryum01@users.noreply.github.com>
2023-10-18 15:43:22 +0200

    Update algebra.java

--- algebra.java
+++ algebra.java
@@ -4,20 +4,20 @@ public class algebra {
      public static void main(String arg[]){
         Scanner scan = new Scanner(System.in);
         System.out.println("inserisci un operatore algebrico");
-        String number =scan.nextLine();
+        char number =scan.next().charAt(0);
         scan.close();
         operator(number);
     }
 
     public static void operator(char a){
         switch(a){
-           case "+" : System.out.println("addizione");
+           case '+' : System.out.println("addizione");
            break;
-           case "-" : System.out.println("sottrazione");
+           case '-': System.out.println("sottrazione");
            break;
-           case "*": System.out.println("moltiplicazione");
+           case '*': System.out.println("moltiplicazione");
            break;
-           case "/" : System.out.println("divisione");
+           case '/' : System.out.println("divisione");
            break;
            default: System.out.println("input non valido");
         }
