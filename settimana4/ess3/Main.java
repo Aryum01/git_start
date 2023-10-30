b4a8dae9a27ebb7364e537e8627384c91afbcde3
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-10-30 20:09:14 +0100

    Add files via upload

---
+++ settimana4/ess3/Main.java
@@ -0,0 +1,19 @@
+package settimana4.ess3;
+
+public class Main {
+     public static void main(String[] args){
+       try {
+            int a = divisione(34, 0);
+            System.out.println("Il risultato della divisione è: " + a);
+        } catch (ArithmeticException e) {
+            System.out.println("Errore: " + e.getMessage());
+        }
+    }
+
+    public static int divisione(int b, int c) {
+        if (c == 0) {
+            throw new ArithmeticException("Divisione per zero non consentita.");
+        }
+        return b / c;
+    }
+}
