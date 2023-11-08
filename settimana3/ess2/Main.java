baa2e67f496e5520f7b70c00a9ceba14adc35f04
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-08 16:09:32 +0100

    Update Main.java

--- settimana3/ess2/Main.java
+++ settimana3/ess2/Main.java
@@ -2,9 +2,8 @@ package settimana3.ess2;
 
 public class Main {
     public static void main(String[] args) {
-        
-        double areaTriangolo = Forma.calcolaArea();
-        double areaRettangolo = Rettangolo.calcolaArea();
-        
+        Rettangolo.calcolaArea();
+
     }
 }
+
