b8cd5a9dc2c377e6e0538a51741191b8c676b214
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-10 11:50:39 +0100

    Add files via upload

---
+++ settimana5/ess3/Fruit.java
@@ -0,0 +1,40 @@
+package java_advanced.settimana5.ess3;
+
+import java.util.LinkedList;
+
+public class Fruit {
+    private static LinkedList<Fruit> fruits = new LinkedList<>();
+    private String name;
+
+    public Fruit(String name) {
+        this.name = name;
+    }
+
+    public static void newFruitsList() {
+        fruits.add(new Fruit("Mela"));
+        fruits.add(new Fruit("Banana"));
+        fruits.add(new Fruit("Pesca"));
+        System.out.println("initial list");
+        printList();
+    }
+
+    public static void addTofruitsList() {
+
+        fruits.addFirst(new Fruit("Pera"));
+        fruits.addLast(new Fruit("Ananas"));
+        System.out.println("new list ");
+        Fruit.printList();
+    }
+
+    public static void printList() {
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit);
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Fruit [name=" + name + "]";
+    }
+
+}
---
+++ settimana5/ess3/Main.java
@@ -0,0 +1,8 @@
+package java_advanced.settimana5.ess3;
+
+public class Main {
+    public static void main(String[] args) {
+        Fruit.newFruitsList();
+        Fruit.addTofruitsList();
+    }
+}
