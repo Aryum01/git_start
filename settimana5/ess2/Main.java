2fd311a9e0140e8c039da701a4fe4304e2b3c99e
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-10 11:29:54 +0100

    Add files via upload

---
+++ settimana5/ess2/Main.java
@@ -0,0 +1,10 @@
+package java_advanced.settimana5.ess2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Student.newArrayList();
+        System.out.println("sorted list by age ");
+        Student.printArray();
+    }
+}
