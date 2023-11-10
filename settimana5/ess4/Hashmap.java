c29540c6945a99a4897b8eb228bb0bef168e7269
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-10 12:37:02 +0100

    Add files via upload

---
+++ settimana5/ess4/Hashmap.java
@@ -0,0 +1,28 @@
+package java_advanced.settimana5.ess4;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Hashmap {
+
+    private static HashMap<String, Integer> firstMap = new HashMap<>();
+
+    private static HashMap<String, Integer> secondMap = new HashMap<>(Map.of("Pesca", 4));
+
+    private static HashMap<String, Integer> thirdMap = new HashMap<>();
+
+    public static void initializeMap() {
+        firstMap.put("Mela", 3);
+        thirdMap.putAll(Map.of("Banana", 7, "Pera", 8));
+        printHashMap(firstMap);
+        printHashMap(secondMap);
+        printHashMap(thirdMap);
+    }
+
+    public static void printHashMap(HashMap<String, Integer> map) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println("Chiave: " + entry.getKey() + ", Valore: " + entry.getValue());
+        }
+        System.out.println();
+    }
+}
---
+++ settimana5/ess4/Main.java
@@ -0,0 +1,7 @@
+package java_advanced.settimana5.ess4;
+
+public class Main {
+    public static void main(String[] args){
+        Hashmap.initializeMap();
+    }
+}
