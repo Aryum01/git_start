7ddd2ae48b825129c59c215b3ac1ee51f96a1ff5
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-15 12:29:10 +0100

    Update Main.java

--- settimana6/ess2/Main.java
+++ settimana6/ess2/Main.java
@@ -1,7 +1,24 @@
-package java_advanced.settimana6.ess2;
+package settimana6.ess2;
+
+import java.time.OffsetDateTime;
+import java.time.format.DateTimeFormatter;
+
 
 public class Main {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        date();
+
+    }
+
+    public static void date() {
+        String dateString = "2023-03-01T13:00:00Z";
+
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString);
+
+        String dayMonthYear = offsetDateTime.format(DateTimeFormatter.ofPattern("dd-MMMM-yyyy"));
+        
+
+        System.out.println(dayMonthYear);
         
     }
 }
