b3fafc2cec394efa2c082552f2f7fc01e016dbd0
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-17 16:27:18 +0100

    Update MainTest.java

--- settimana6/ess9/MainTest.java
+++ settimana6/ess9/MainTest.java
@@ -2,13 +2,24 @@ package settimana6.ess9;
 
 import static org.junit.Assert.assertEquals;
 
+import java.time.OffsetDateTime;
+import java.time.format.TextStyle;
+import java.util.Locale;
+
 import org.junit.Test;
 
 public class MainTest {
     @Test
     public void testDate() {
-        String actString = Main.date();
-        String expString = "Anno: 2023\nMese: 3\nGiorno: 1\nGiorno della Settimana: mercoledì";
-        assertEquals(expString, actString);
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse("2023-03-01T13:00:00Z");
+
+        int year = offsetDateTime.getYear();
+        int month = offsetDateTime.getMonthValue();
+        int day = offsetDateTime.getDayOfMonth();
+        String dayOfWeek = offsetDateTime.getDayOfWeek().getDisplayName(TextStyle.FULL, Locale.getDefault());
+        assertEquals(2023, year);
+        assertEquals(3, month);
+        assertEquals(1, day);
+        assertEquals("mercoledì", dayOfWeek);
     }
 }
