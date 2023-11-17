5545765152367bd43a546fc9e45d5ca0afe176f8
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-17 16:15:06 +0100

    Update Main.java

--- settimana6/ess10/Main.java
+++ settimana6/ess10/Main.java
@@ -1,27 +1,29 @@
 package settimana6.ess10;
 
+
 import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
+import java.time.format.FormatStyle;
+import java.time.temporal.ChronoUnit;
 import java.util.Locale;
 
 public class Main {
-    public static void main(String[] args) {
-        date();
 
+    public static void main(String[] args) {
+        date(null);
     }
 
-    public static String date() {
-        String dateString = "2023-03-01T13:00:00Z";
+    public static OffsetDateTime date(String inputDateString) {
 
-        OffsetDateTime offsetDateTime = OffsetDateTime.parse(dateString);
+        OffsetDateTime offsetDateTime = OffsetDateTime.parse(inputDateString);
+        OffsetDateTime addYear = offsetDateTime.plus(1, ChronoUnit.YEARS);
+        OffsetDateTime RemoveMonth = addYear.minus(1, ChronoUnit.MONTHS);
+        OffsetDateTime addDays = RemoveMonth.plus(7, ChronoUnit.DAYS);
 
-        OffsetDateTime addYear = offsetDateTime.plusYears(1);
-        OffsetDateTime RemoveMonth = offsetDateTime.minusMonths(1);
-        OffsetDateTime addDay = offsetDateTime.plusDays(7);
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMMM yyyy HH:mm:ss").withLocale(Locale.ITALY);
+        return addDays;
+    }
 
-        return "Nuovo anno: " + addYear.format(formatter) + "\nMese scorso: " + RemoveMonth.format(formatter)
-                + "\nFra 7 giorni: " + addDay.format(formatter);
-
+    public static String format(OffsetDateTime manipulatedDate) {
+        return manipulatedDate.format(DateTimeFormatter.ofLocalizedDate(FormatStyle.FULL).withLocale(Locale.ITALY));
     }
 }
