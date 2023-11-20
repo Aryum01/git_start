f58bb7701c961b07108e825101af46347cf6f6c9
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-20 10:19:11 +0100

    Add files via upload

---
+++ settimana3/ess2/Forma.java
@@ -0,0 +1,20 @@
+package java_basics.settimana3.ess2;
+
+public class Forma {
+    private int base;
+    private int altezza;
+
+    
+
+    public Forma(int base, int altezza) {
+        this.base = base;
+        this.altezza = altezza;
+    }
+
+
+
+    public  void calcolaArea() {
+        System.out.println("Calcolo dell'area generica della forma");
+    }
+
+}
