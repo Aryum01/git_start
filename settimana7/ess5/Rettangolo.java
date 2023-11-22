22b5c2d70b97df8fbe7de90e5f7aa8b831858163
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-22 14:42:07 +0100

    Update Rettangolo.java

--- settimana7/ess5/Rettangolo.java
+++ settimana7/ess5/Rettangolo.java
@@ -3,7 +3,7 @@ package java_advanced.settimana7.ess5;
 public class Rettangolo implements Shape {
     @Override
     public void draw() {
-        System.out.println("Disegna un rettangolo");
+        System.out.println("rettangolo");
     }
 
 }
