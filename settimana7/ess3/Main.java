72d4dc57e62c770ba392e4d507ba0a513ab50ba4
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-11-22 11:11:50 +0100

    Add files via upload

---
+++ settimana7/ess3/Main.java
@@ -0,0 +1,20 @@
+package java_advanced.settimana7.ess3;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 100; i++) {
+            System.out.println("Valore: " + i);
+            System.out.println(checkValue(i));
+        }
+    }
+
+    private static String checkValue(int value) {
+        String result = value % 5 == 0 ? " " + "FizzBuzz": " " + "Buzz";
+         return result;
+
+       
+    }
+}
+
+
+ 
