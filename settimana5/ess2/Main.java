3ce99f6298428bffb6910b1270251bbb2a56aac0
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 16:50:11 +0100

    Update Main.java

--- settimana5/ess2/Main.java
+++ settimana5/ess2/Main.java
@@ -1,10 +1,41 @@
 package settimana5.ess2;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
-        Student.newArrayList();
+        List<Student> students = new ArrayList<>();
+        newArrayList(students);
         System.out.println("sorted list by age ");
-        Student.printArray();
+        printArray(students);
     }
+
+    public static void newArrayList(List<Student> students) {
+        students.add(new Student("Mario", 24));
+        students.add(new Student("Carlo", 22));
+        students.add(new Student("Simone", 25));
+        students.add(new Student("Antonio", 24));
+        students.add(new Student("Danilo", 23));
+        students.add(new Student("Pietro", 29));
+        students.add(new Student("Marco", 25));
+        students.add(new Student("Giuseppe", 29));
+        students.add(new Student("Noemi", 26));
+        students.add(new Student("Francesca", 22));
+        students.add(new Student("Simona", 20));
+        students.add(new Student("Rosanna", 20));
+        System.out.println("initial list");
+        printArray(students);
+        Comparator<Student> SortArray = Comparator.comparingInt(Student::getAge);
+        Collections.sort(students, SortArray);
+    }
+
+    public static void printArray( List<Student> students) {
+        for (Student student2 : students) {
+            System.out.println(student2);
+        }
+    }
+
 }
