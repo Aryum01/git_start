dc09ab750dfc2981768d9e93edd11e341861546c
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 16:42:19 +0100

    Update Main.java

--- settimana5/ess1/Main.java
+++ settimana5/ess1/Main.java
@@ -1,15 +1,32 @@
-package java_advanced.settimana5.ess1;
+package settimana5.ess1;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        Student.newArrayList();
+        List<Student> students = new ArrayList<>();
+        System.out.println("initial list");
+        students.add(new Student("Mario", 24));
+        students.add(new Student("Carlo", 22));
+        students.add(new Student("Simone", 25));
+        printArray(students);
 
-        Student.addStudent(new Student("Giovanni", 26));
-        Student.addStudent(new Student("Pietro", 29));
-        Student.addStudent(new Student("Danilo", 23));
-        Student.addStudent(new Student("Antonio", 27));
-      
+        addStudent(students, new Student("Giovanni", 26));
+        addStudent(students, new Student("Pietro", 29));
+        addStudent(students, new Student("Danilo", 23));
+        addStudent(students, new Student("Antonio", 27));
+
         System.out.println("list with new students");
-        Student.printArray();
+        printArray(students);
+    }
+    public static void addStudent(List<Student> students, Student newStudents) {
+        students.add(newStudents);
+    }
+
+    public static void printArray(List<Student> students) {
+        for (Student student2 : students) {
+            System.out.println(student2);
+        }
     }
 }
