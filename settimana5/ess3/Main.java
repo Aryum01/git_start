9921d2997e9ca638a4d0471527338a91d4bda8d3
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 16:54:49 +0100

    Update Main.java

--- settimana5/ess3/Main.java
+++ settimana5/ess3/Main.java
@@ -1,8 +1,28 @@
 package settimana5.ess3;
 
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
-        Fruit.newFruitsList();
-        Fruit.addTofruitsList();
+        LinkedList<Fruit> fruits = new LinkedList<>();
+        fruits.add(new Fruit("Mela"));
+        fruits.add(new Fruit("Banana"));
+        fruits.add(new Fruit("Pesca"));
+        System.out.println("initial list");
+        printList(fruits);
+        addTofruitsList(fruits);
+        System.out.println("new list ");
+        printList(fruits);
+    }
+
+    public static void addTofruitsList(LinkedList<Fruit> fruits) {
+        fruits.addFirst(new Fruit("Pera"));
+        fruits.addLast(new Fruit("Ananas"));
+    }
+
+    public static void printList(LinkedList<Fruit> fruits) {
+        for (Fruit fruit : fruits) {
+            System.out.println(fruit);
+        }
     }
 }
