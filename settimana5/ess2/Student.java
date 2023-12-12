a694e24b8d95792c8ff7e37123b6cef5a93dcf42
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 16:50:40 +0100

    Update Student.java

--- settimana5/ess2/Student.java
+++ settimana5/ess2/Student.java
@@ -1,12 +1,7 @@
 package settimana5.ess2;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
 public class Student {
-    private static List<Student> students = new ArrayList<>();
+
     private String name;
     private int age;
 
@@ -21,33 +16,6 @@ public class Student {
 
     public int getAge() {
         return age;
-    }
-
-    public static void newArrayList() {
-
-        students.add(new Student("Mario", 24));
-        students.add(new Student("Carlo", 22));
-        students.add(new Student("Simone", 25));
-        students.add(new Student("Antonio", 24));
-        students.add(new Student("Danilo", 23));
-        students.add(new Student("Pietro", 29));
-        students.add(new Student("Marco", 25));
-        students.add(new Student("Giuseppe", 29));
-        students.add(new Student("Noemi", 26));
-        students.add(new Student("Francesca", 22));
-        students.add(new Student("Simona", 20));
-        students.add(new Student("Rosanna", 20));
-        System.out.println("initial list");
-        printArray();
-        Comparator<Student> SortArray = Comparator.comparingInt(Student::getAge);
-        Collections.sort(students, SortArray);
-
-    }
-
-    public static void printArray() {
-        for (Student student2 : students) {
-            System.out.println(student2);
-        }
     }
 
     @Override
