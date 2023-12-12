91af0bebcfa681658389af14381dac1d0ff108df
Aryum01 <scalisidanilo20@gmail.com>
2023-12-12 17:17:22 +0100

    inutile

--- Area_triangolo.java
+++
@@ -1,9 +0,0 @@
-public class Area_triangolo{
-    public static void main(String arg[]){
-       double base = 5;
-       double altezza = 6;
-       double   area = (base * altezza)/2;
-
-       System.out.println("ecco l'area del triangolo :" +  area);
-    }
-}
--- valore_ascii.java
+++
@@ -1,8 +0,0 @@
-public class valore_ascii {
-    public static void main(String arg[]){
-        char a,b;
-        a = 'a';
-        b = 'b';
-        System.out.println( a + b);
-    }
-}
