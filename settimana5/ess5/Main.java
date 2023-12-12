1eb6c37b7423740c708b286d6c7b6ef02259261c
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 17:05:55 +0100

    Update Main.java

--- settimana5/ess5/Main.java
+++ settimana5/ess5/Main.java
@@ -1,7 +1,21 @@
 package settimana5.ess5;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Main {
-    public static void main(String[] args){
-            Treemap.inizializeTreemap();
+    public static void main(String[] args) {
+        TreeMap<String, Integer> treeMap = new TreeMap<>();
+        treeMap.put("Pesca", 4);
+        treeMap.put("Mela", 3);
+        treeMap.put("Pera", 8);
+        printTreeMap(treeMap);
+    }
+
+    public static void printTreeMap(TreeMap<String, Integer> treeMap) {
+        for (Map.Entry<String, Integer> entry : treeMap.entrySet()) {
+            System.out.println("Chiave: " + entry.getKey() + ", Valore: " + entry.getValue());
+        }
+        System.out.println();
     }
 }
