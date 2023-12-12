1a2e32d079bf2a5d84c4be8a426be0a4a5071b1d
Scalisi Danilo <147306723+Aryum01@users.noreply.github.com>
2023-12-12 16:59:55 +0100

    Update Main.java

--- settimana5/ess4/Main.java
+++ settimana5/ess4/Main.java
@@ -1,7 +1,26 @@
 package settimana5.ess4;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
-    public static void main(String[] args){
-        Hashmap.initializeMap();
+    public static void main(String[] args) {
+        HashMap<String, Integer> firstMap = new HashMap<>();
+
+        HashMap<String, Integer> secondMap = new HashMap<>(Map.of("Pesca", 4));
+
+        HashMap<String, Integer> thirdMap = new HashMap<>();
+        firstMap.put("Mela", 3);
+        thirdMap.putAll(Map.of("Banana", 7, "Pera", 8));
+        printHashMap(firstMap);
+        printHashMap(secondMap);
+        printHashMap(thirdMap);
+    }
+    
+    public static void printHashMap(HashMap<String, Integer> map) {
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            System.out.println("Chiave: " + entry.getKey() + ", Valore: " + entry.getValue());
+        }
+        System.out.println();
     }
 }
