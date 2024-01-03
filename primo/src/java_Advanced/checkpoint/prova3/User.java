15d6da16e854f5a000d41840280e5a81457ca746
Aryum01 <scalisidanilo20@gmail.com>
2024-01-03 19:12:24 +0100

    un altro esempio

---
+++ primo/src/java_Advanced/checkpoint/prova3/User.java
@@ -0,0 +1,9 @@
+package java_Advanced.checkpoint.prova3;
+
+public interface User {
+    public String getName();
+
+    public String getSurname();
+
+}
+
---
+++ primo/src/java_Advanced/checkpoint/prova3/UserManager.java
@@ -0,0 +1,33 @@
+package java_Advanced.checkpoint.prova3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserManager {
+    private List<User> usersList = new ArrayList<>();
+
+    public void addUser(User users) {
+        usersList.add(users);
+    }
+
+    public void removeUser(User users) {
+        usersList.remove(users);
+    }
+
+    public void printUsers() {
+        for (User user : usersList) {
+            System.out.println(user.getName() + " " + user.getSurname());
+        }
+        System.out.println(" ");
+    }
+
+    public List<User> livelUser(UserLivel livel) {
+        List<User> userLivel = new ArrayList<>();
+        for (User user : usersList) {
+            if (user instanceof PremiumUser premiumUser && premiumUser.getLivel() == livel) {
+                userLivel.add(user);
+            }
+        }
+        return userLivel;
+    }
+}
