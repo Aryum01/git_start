cced675b0fbdc07136aea77edd90876665952b9e
Aryum01 <scalisidanilo20@gmail.com>
2024-01-16 19:58:14 +0100

    second ess

--- spring/demo/src/main/java/com/example/demo/settimana1/ess2/DemoApplication.java
+++ spring/demo/src/main/java/com/example/demo/settimana1/ess2/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.demo.settimana1.ess2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
