a028f237f346b690d276095c882399083325b6ec
Aryum01 <scalisidanilo20@gmail.com>
2024-01-25 12:52:57 +0100

    add cors

---
+++ spring/demo/src/main/java/com/example/demo/settimana2/ess2/MyConfiguration.java
@@ -0,0 +1,25 @@
+package com.example.demo.settimana2.ess2;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+
+import java.util.Arrays;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:8080"));
+        configuration.setAllowedMethods(Arrays.asList("POST"));
+        configuration.setAllowedMethods(Arrays.asList("GET"));
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
