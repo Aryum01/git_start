a0453bcfb7662af0478a88d69df09ff58e346968
Aryum01 <scalisidanilo20@gmail.com>
2024-02-26 18:12:03 +0100

    logging exercise

---
+++ logging/logging/src/main/java/com/example/logging/LoggingController.java
@@ -0,0 +1,28 @@
+package com.example.logging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoggingController {
+
+    @Autowired
+    LoggingServices loggingServices;
+
+    @GetMapping("/")
+    public String welcome() {
+        return loggingServices.welcome();
+    }
+
+    @GetMapping("/exp")
+    public double calculateExponent() {
+        return loggingServices.exponent();
+    }
+
+    @GetMapping("/get-errors")
+    public String getErrors() {
+        return loggingServices.errors();
+    }
+}
